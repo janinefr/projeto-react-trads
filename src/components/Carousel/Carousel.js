7d077778e686edc3bec5248e9925589d2b2fb3c5
janinefreire18@gmail.com <janinefreire18@gmail.com>
2023-12-07 20:43:19 -0300

    sessao carrossel

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -3,16 +3,17 @@ import { Container } from '../../globalStyles';
 import { TextWrapper, TopLine, Heading, Group1, Group2, CarouselSection, } from '../Carousel/CarouselStyles';
 import group1 from '../../assets/Group1.png';
 import group2 from '../../assets/Group2.png';
-import video from '../../assets/video-player.png';
-import arrowLeft from '../../assets/arrow-btnL.png';
-import arrowRight from '../../assets/arrow-btnR.png';
+// import video from '../../assets/video-player.png';
+// import arrowLeft from '../../assets/arrow-btnL.png';
+// import arrowRight from '../../assets/arrow-btnR.png';
 import { GoStarFill } from "react-icons/go";
 import { register } from 'swiper/element/bundle'
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
-import carouselData from './CarouselData';
+// import carouselData from './CarouselData';
+import { Swiper, SwiperSlide } from 'swiper/react'
 
 register();
 
@@ -21,19 +22,39 @@ register();
 
 const Carousel = () => {
 
-    const carousel = useRef(null);
+    // const carousel = useRef(null);
 
-    const handleLeftClick = (e) => {
-        e.preventDefault();
-        console.log(carousel.current.offsetWidth);
+    // const handleLeftClick = (e) => {
+    //     e.preventDefault();
+    //     console.log(carousel.current.offsetWidth);
 
-    };
+    // };
 
-    const handleRightClick = (e) => {
-        e.preventDefault();
-        console.log(carousel.current.offsetWidth);
+    // const handleRightClick = (e) => {
+    //     e.preventDefault();
+    //     console.log(carousel.current.offsetWidth);
 
-    };
+    // };
+
+    const data = [
+        {
+            id: '1',
+            name: 'Isabela Fontes',
+            description: 'Leo diam egestas aliquet etiam sed odio tortor sit. Ultricies feugiat mauris tempor odio eu massa amet sagittis. Fringilla enim vel arcu amet, condimentum sagittis. Lectus consequat pellentesque nibh convallis lacus, id. Orci, morbi sagittis, vel lobortis. Felis pellentesque maecenas aliquam turpis velit aliquam diam eget. Tincidunt amet aliquam ultricies nec. Commodo cras fringilla sed morbi velit semper iaculis. Vitae, lacinia nisl, sed velit orci. Nunc feugiat pellentesque pretium convallis. Arcu netus non, nunc et est sed venenatis in. Ut vulputate viverra quisque turpis nulla. Facilisis blandit sagittis, laoreet quis amet..',
+            icons: 5,
+            image: require('../../assets/video-player.png'),
+        },
+
+        {
+            id: '2',
+            name: 'Isabela Fontes',
+            description: 'Fringilla enim vel arcu amet, condimenamet.',
+            icons: 5,
+            image: require('../../assets/video-player.png'),
+        }
+
+
+    ]
 
 
     return (
@@ -44,9 +65,47 @@ const Carousel = () => {
                     <Heading>O que nossos clientes estão dizendo</Heading>
                 </TextWrapper>
 
+                <div className='container'>
+                    <Swiper
+
+                        slidesPerView={1}
+                        pagination={{ clickable: true }}
+                        navigation
+
+                    >
 
 
-                <div className='carousel' ref={carousel}>
+                        {data.map((item) => (
+                            <SwiperSlide key={item.id}>
+
+                                <div className='item'>
+                                    <img src={item.image} alt="video" className='slide_image' />
+                                    <div className='slide_text'>
+                                        <span className='slide_text1' >{item.name}</span>
+                                        <span className='slide_text2'>{item.description}</span>
+                                    </div>
+                
+                                    
+                                </div>
+                                <div className='icons'>
+                                        {Array.from({ length: item.icons }).map((_, index) => (
+                                            <GoStarFill key={index} />
+                                        ))}
+                                    </div>
+
+                            </SwiperSlide>
+
+
+
+                        ))}
+                    </Swiper>
+
+                </div>
+
+
+
+
+                {/* <div className='carousel' ref={carousel}>
                     <div className='container'>
                         <div className='buttons'>
                             <button onClick={handleLeftClick}><img src={arrowLeft} alt="left" /></button>
@@ -72,7 +131,7 @@ const Carousel = () => {
                         ))}
 
                     </div>
-                </div>
+                </div> */}
 
 
 
--- src/components/Carousel/CarouselStyles.js
+++ src/components/Carousel/CarouselStyles.js
@@ -15,12 +15,12 @@ export const CarouselSection = styled.section`
     box-shadow: inset 0 0 0 1000px rgba(0, 0, 0, 0.2);
     z-index: -1;
 
-    .container{
+    .container {
         position: relative;
         display: flex;
         justify-content: center;
         align-items: center;
-        flex-direction: row-reverse; 
+        flex-direction: row-reverse;
         gap: 24px;
         max-width: 100vw;
         height: 510px;
@@ -30,109 +30,132 @@ export const CarouselSection = styled.section`
         backdrop-filter: blur(10px);
         padding-right: 50px;
         padding-left: 50px;
-        z-index:1;
-        
-    }
-
-    .container .carousel{
-        display: flex;
-        justify-content: center; 
-        align-items: center; 
-        flex-direction: row-reverse; 
-        overflow-x: auto;
-        width: 100%;
-        scroll-behavior: smooth;
-
-    }
-
-    .container .carousel::-webkit-scrollbar{
-        display: none;
-    }
-
-    .container .carousel .item {
-        display: flex;
-        justify-content: center; 
-        align-items: center; 
-        flex-direction: row; 
-        flex:none;
-
-      }
-    
-
-    .container .carousel .item .image img{
-        max-width: 30vw;
-        height: 100%;
-        object-fit: cover;
-        position: relative;
-        overflow: hidden;
-    }
-
-    .container .carousel .item .info{
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        max-width: 50%;
-        margin-left: 1rem;
-        margin-top: 1rem;
-        
-    }
-
-     .info span{
-        display: block;
-        text-align: left;
-        color: white;
-        padding: 5px;
-    
-    }
-    
-    .name{
-        font-size: 1.2rem;
-        margin: 10px 0;
-        font-family: Work Sans;
-        font-style: normal;
-        font-weight: 600;
-        line-height: 27px; /* 112.5% */
-        letter-spacing: 0.518px;
-    }
-
-    .description{
-        font-family: Work Sans;
-        font-size: 0.8rem;
-        font-style: normal;
-        font-weight: 400;
-        line-height: 24px; /* 150% */
-        letter-spacing: 0.307px;
-        
-    }
-
-    .icons{
-        display: flex;
-        align-items: flex-start;
-        gap: 8px;
-        color: #F3C141;
-    }
-
-    .buttons {
-        position: absolute;
-        top: 50%; /* Ajuste conforme necessário para posicionar verticalmente */
-        left: 0;
-        right: 0;
-        transform: translateY(-50%); /* Centraliza verticalmente */
-        display: flex;
-        justify-content: space-between;
-        width: 100%;
         z-index: 1;
-      }
+    
       
-      .buttons button {
-        background-color: transparent;
-        border: none;
-        cursor: pointer;
+        .item {
+            max-width: 100%;
+            display: flex;
+            justify-content: center;
+            flex-direction: row;
+            align-items: center;
+            object-fit: cover;
+          }   
+
+          .slide_image{
+            width: 30vw;
+          }
+
+          .slide_text{
+            display: flex;
+            justify-content: center;
+            flex-direction: column;
+            align-items: left;
+            margin: 30px;
+
+          }
+        }
 
 
-      img{
-        max-width: 50%;
-      }
+    // .container .carousel{
+    //     display: flex;
+    //     justify-content: center; 
+    //     align-items: center; 
+    //     flex-direction: row-reverse; 
+    //     overflow-x: auto;
+    //     width: 100%;
+    //     scroll-behavior: smooth;
+
+    // }
+
+    // .container .carousel::-webkit-scrollbar{
+    //     display: none;
+    // }
+
+    // .container .carousel .item {
+    //     display: flex;
+    //     justify-content: center; 
+    //     align-items: center; 
+    //     flex-direction: row; 
+    //     flex:none;
+
+    //   }
+    
+
+    // .container .carousel .item .image img{
+    //     max-width: 30vw;
+    //     height: 100%;
+    //     object-fit: cover;
+    //     position: relative;
+    //     overflow: hidden;
+    // }
+
+    // .container .carousel .item .info{
+    //     display: flex;
+    //     flex-direction: column;
+    //     justify-content: space-between;
+    //     max-width: 50%;
+    //     margin-left: 1rem;
+    //     margin-top: 1rem;
+        
+    // }
+
+    //  .info span{
+    //     display: block;
+    //     text-align: left;
+    //     color: white;
+    //     padding: 5px;
+    
+    // }
+    
+    // .name{
+    //     font-size: 1.2rem;
+    //     margin: 10px 0;
+    //     font-family: Work Sans;
+    //     font-style: normal;
+    //     font-weight: 600;
+    //     line-height: 27px; /* 112.5% */
+    //     letter-spacing: 0.518px;
+    // }
+
+    // .description{
+    //     font-family: Work Sans;
+    //     font-size: 0.8rem;
+    //     font-style: normal;
+    //     font-weight: 400;
+    //     line-height: 24px; /* 150% */
+    //     letter-spacing: 0.307px;
+        
+    // }
+
+    // .icons{
+    //     display: flex;
+    //     align-items: flex-start;
+    //     gap: 8px;
+    //     color: #F3C141;
+    // }
+
+    // .buttons {
+    //     position: absolute;
+    //     top: 50%; /* Ajuste conforme necessário para posicionar verticalmente */
+    //     left: 0;
+    //     right: 0;
+    //     transform: translateY(-50%); /* Centraliza verticalmente */
+    //     display: flex;
+    //     justify-content: space-between;
+    //     width: 100%;
+    //     z-index: 1;
+    //   }
+      
+    //   .buttons button {
+    //     background-color: transparent;
+    //     border: none;
+    //     cursor: pointer;
+
+
+    //   img{
+    //     max-width: 50%;
+    //   }
 
       
       
